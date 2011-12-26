f38a954f8926cbf77312a9ee2b5f890f8c8a0bfd
Samuel Volk <samivolk16@hotmail.com>
2011-12-26 16:08:52 -0500

    added tile info command

--- main.c
+++ main.c
@@ -26,6 +26,7 @@
 // 'S' = 83		Shop
 #define shop		83
 // '&' = 38		Debris
+//#define debris ##
 
 // STANDARD HEADERS
 #include <stdio.h>
@@ -39,51 +40,53 @@ void printl()
 	printf("\n");
 }
 
-/*
-void prints(char *array, int length)
-{
-	int character = 0;
-
-	for (character = 0; character; )
-	{
-		printf(array[]);
-	}
-}
-*/
 
 ////////////////
 //  COMMANDS  //
 ////////////////////
 
-/////////
-// MAP //
+//// MAP
 //	- "map" command
 //	- prints the map
-void print_map(char map[20][20], int char_position[2])
+void print_map(char map[20][20], int char_position[2], char *word[100])
 {
 
-	printf("Here is the map: \n");
-	for (int i = 0; i < 20; i++)
+	if (word[1] == NULL)
 	{
-		for(int j = 0; j < 20; j++)
+		printf("Here is the map: \n");
+		for (int i = 0; i < 20; i++)
 		{
-			if ((char_position[0] == i) && (char_position[1] == j))
-				printf("%c", character);
-			else
-				printf("%c", map[i][j]);
+			for(int j = 0; j < 20; j++)
+			{
+				if ((char_position[0] == i) && (char_position[1] == j))
+					printf("%c", character);
+				else
+					printf("%c", map[i][j]);
+			}
+		printl();
 		}
-	printl();
 	}
-
+	else if (strcmp("legend", word[1]) == 0)
+	{
+		printf("Map legend:\n"  
+		       "#	Player\n" 
+		       "=	W/E road\n"  
+		       "|	N/S road\n" 
+		       "+	Intersection\n" 
+		       "o	Round-about\n" 
+		       "H	Hospital\n" 
+		       "P	Police Station\n" 
+		       "S	Shop\n" 
+		       "&	Debris\n");
+	}
 }
 
-//////////
-// WALK //
+//// WALK
 //	- "walk [direction]" command
 //	- moves character depending on the direction
-int walk(char *direction, int char_position[2])
+int move(char *direction, int char_position[2])
 {
-	printf("Direction in walk(): %s\n", direction);
+	//printf("Direction in walk(): %s\n", direction);
 
 	if (direction == NULL)
 	{
@@ -105,6 +108,44 @@ int walk(char *direction, int char_position[2])
 
 }
 
+//// Tile Info
+//	- gives the user information about the tile
+void tile_info(char map[20][20], int char_position[2])
+{
+
+	printf("tile: %c\n", map[char_position[0]][char_position[1]]);
+
+	switch (map[char_position[0]][char_position[1]])
+	{
+		case '=':
+			printf("You are currently on a road that goes from the West to East end of the city\n");
+			break;
+		case '|':
+			printf("You are currently on a road that goes from the North to South end of the city\n");
+			break;
+		case '+':
+			printf("You are at an intersection\n");
+			break;
+		case 'o':
+			printf("You are at a round-a-bout\n");
+			break;
+		case 'H':
+			printf("You are in a city hospital\n");
+			break;
+		case 'P':
+			printf("You are in a city police station\n");
+			break;
+		case 'S':
+			printf("You are in a shop\n");
+			break;
+		case '&':
+			printf("You are standing in a pile of debris.\n");
+			break;
+		default:
+			printf("Map tile not recognized.\n");
+	}
+}
+
 ////////////////////
 
 // get_command
@@ -118,10 +159,11 @@ void get_command(char command[100])
 	//scanf("%*c");
     fflush(stdin);
 
+	// remove the newline character
 	if (command[strlen(command) - 1] == '\n')
 		command[strlen(command) - 1] = '\0';
 
-	printf("Right after getting: %s\n", command);
+	//printf("Right after getting: %s\n", command);
 
 }
 
@@ -138,11 +180,6 @@ void split_command(char command[100],char *word[100])
 	word[0] = strtok(command, " ");
 	// The rest
 	while ((word[i] = strtok(NULL, " ")) != NULL)	i++;			// NOTE: strtok will return NULL when it reaches the end
-//	{
-	//	word[i] = strtok(NULL, " ");
-    //	printf("Found: %s\n", word[i]);
-	//	i++;
-//	}
 
 }
 
@@ -151,21 +188,21 @@ void split_command(char command[100],char *word[100])
 int check_command(char *word[100], char map[20][20], int char_position[2])
 {
 
-	printf("check_command word[0]: %s\n", word[0]);
-	printf("check_command word[1]: %s\n", word[1]);
+	//printf("check_command word[0]: %s\n", word[0]);
+	//printf("check_command word[1]: %s\n", word[1]);
+	//printf("check_command word[2]: %s\n", word[2]);
 
 	if (word[0] == NULL)
 		return 1;
 	//MAP
 	if (strcmp("map", word[0]) == 0)
-		print_map(map, char_position);
+		print_map(map, char_position, word);
 	//WALK
 	if (strcmp("walk", word[0]) == 0)
-	{
-		walk(word[1], char_position);
-	}
-
-
+		move(word[1], char_position);
+	//TILE INFO
+	if ((strcmp("tile", word[0]) == 0) && (strcmp("info", word[1]) == 0))
+		tile_info(map, char_position);
 	
 	return 0;
 }
