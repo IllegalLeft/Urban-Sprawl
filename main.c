5cd449da02520463c7a1490028e5e3faba465c1b
Samuel Volk <samivolk16@hotmail.com>
2012-01-02 20:56:47 -0500

    basic hospitals, player characteristics

--- main.c
+++ main.c
@@ -33,6 +33,19 @@
 #include <ctype.h>
 #include <string.h>
 
+struct playercharacteristics
+{
+	//char *name;
+	int position[2];
+	char map[20][20];
+
+	int health[2];
+	//int intelligence;
+	//int stealth;
+	
+	//int weapons[5];
+};
+
 // printl
 //	- prints a new line only
 void printl()
@@ -55,125 +68,7 @@ void pause()
 	fflush(stdin);
 }
 
-
 ////////////////
-//  COMMANDS  //
-////////////////
-
-//// MAP
-//	- "map" command
-//	- prints the map
-void print_map(char map[20][20], int char_position[2], char *word[100])
-{
-
-	if (word[1] == NULL)
-	{
-		printf("Here is the map: \n");
-		for (int i = 0; i < 20; i++)
-		{
-			for(int j = 0; j < 20; j++)
-			{
-				if ((char_position[0] == i) && (char_position[1] == j))
-					printf("%c", character);
-				else
-					printf("%c", map[i][j]);
-			}
-			printl();
-		}
-		pause();
-
-	}
-	else if (strcmp("legend", word[1]) == 0)
-	{
-		printf("Map legend:\n"  
-		       "#	Player\n" 
-		       "=	W/E road\n"  
-		       "|	N/S road\n" 
-		       "+	Intersection\n" 
-		       "o	Round-about\n" 
-		       "H	Hospital\n" 
-		       "P	Police Station\n" 
-		       "S	Shop\n" 
-		       "&	Debris\n");
-		pause();
-	}
-}
-
-//// WALK
-//	- "walk [direction]" command
-//	- moves character depending on the direction
-int move(char *direction, int char_position[2])
-{
-	//printf("Direction in walk(): %s\n", direction);
-
-	if (direction == NULL)
-	{
-		printf("Please specify a direction.\n");
-		pause();
-		return 1;
-	}
-	if (((strcmp("up", direction) == 0) || (strcmp("north", direction) == 0)) && (char_position[0] != 0))
-		char_position[0]--;
-	else if (((strcmp("down", direction) == 0) || (strcmp("south", direction) == 0)) && (char_position[0] != 19))
-		char_position[0]++;
-	else if (((strcmp("left", direction) == 0) || (strcmp("west", direction) == 0)) && (char_position[1] != 0))
-		char_position[1]--;
-	else if (((strcmp("right", direction) == 0) || (strcmp("east", direction) == 0)) && (char_position[1] != 19))
-		char_position[1]++;
-	else
-	{
-		printf("Direction not recognized\n");
-		pause();
-	}
-
-	return 0;
-
-}
-
-//// Tile Info
-//	- gives the user information about the tile
-void tile_info(char map[20][20], int char_position[2])
-{
-
-	char current_tile = map[char_position[0]][char_position[1]];
-
-	printf("tile: %c\n", current_tile);
-
-	switch (current_tile)
-	{
-		case '=':
-			printf("You are currently on a road that goes from the West to East end of the city\n");
-			break;
-		case '|':
-			printf("You are currently on a road that goes from the North to South end of the city\n");
-			break;
-		case '+':
-			printf("You are at an intersection\n");
-			break;
-		case 'o':
-			printf("You are at a round-a-bout\n");
-			break;
-		case 'H':
-			printf("You are in a city hospital\n");
-			break;
-		case 'P':
-			printf("You are in a city police station\n");
-			break;
-		case 'S':
-			printf("You are in a shop\n");
-			break;
-		case '&':
-			printf("You are standing in a pile of debris.\n");
-			break;
-		default:
-			printf("Map tile not recognized.\n");
-
-	}
-
-	pause();
-}
-
-////////////////////
 
 // get_command
 //	- receives the command from the user
@@ -210,9 +105,228 @@ void split_command(char command[100],char *word[100])
 
 }
 
+////////////////
+//  COMMANDS  //
+////////////////
+
+//// ENTER
+//	- "enter" command
+//	- enters the building on the current tile
+int enter(char map[20][20], int position[2], int health[])
+{
+
+	char current_tile = map[position[0]][position[1]];
+	char command[100];
+	char *word[100];
+
+	switch (current_tile)
+	{
+		case 'H':
+			while (1)
+			{
+				printf("You have entered the hospital.\n");
+				printf("Healing yourself will cost: $10.\n");
+				
+				get_command(command);
+				split_command(command, word);
+
+				if (strcmp("heal", word[0]) == 0)
+				{
+					printf("This will cost you: $10. Carry on?  (yes / no)\n");
+					get_command(command);
+					split_command(command, word);
+
+					if (strcmp("yes", word[0]) == 0)
+					{
+						//Take money
+						
+						//Heal
+						health[0] = health[1];
+						printf("You have been healed! (NOT REALLY)\n");
+					}
+				}
+				else if (strcmp("exit", word[0]) == 0)
+				{
+					printf("You have exited the building\n");
+					return 0;
+				}
+				else
+				{
+					printf("Command not recognized.\n");
+					pause();
+				}
+					
+			}
+			break;
+		case 'P':
+			while (1)
+			{
+				printf("You have entered the police station.\n");
+				return 0;
+			}
+			break;
+		case 'S':
+			while (1)
+			{
+				printf("You have entered the shop.\n");
+
+				get_command(command);
+				split_command(command, word);
+
+				if (strcmp("", word[0]) == 0)
+				{
+					printf("This will cost you: $10. Carry on?  (yes / no)\n");
+					get_command(command);
+					split_command(command, word);
+
+					if (strcmp("yes", word[0]) == 0)
+					{
+						//Heal
+						printf("You have been healed! (NOT REALLY)\n");
+					}
+				}
+				else if (strcmp("exit", word[0]) == 0)
+				{
+					printf("You have exited the building\n");
+					return 0;
+				}
+				else
+				{
+					printf("Command not recognized.\n");
+					pause();
+				}
+
+				return 0;
+			}
+			break;
+		default:
+			printf("Cannot enter the current tile.\n");
+		return 0;
+	}
+	
+}
+
+//// MAP
+//	- "map" command
+//	- prints the map
+void print_map(char map[20][20], int position[2], char *word[100])
+{
+
+	if (word[1] == NULL)
+	{
+		printf("Here is the map: \n");
+		for (int i = 0; i < 20; i++)
+		{
+			for(int j = 0; j < 20; j++)
+			{
+				if ((position[0] == i) && (position[1] == j))
+					printf("%c", character);
+				else
+					printf("%c", map[i][j]);
+			}
+			printl();
+		}
+		pause();
+
+	}
+	else if (strcmp("legend", word[1]) == 0)
+	{
+		printf("Map legend:\n"  
+		       "#	Player\n" 
+		       "=	W/E road\n"  
+		       "|	N/S road\n" 
+		       "+	Intersection\n" 
+		       "o	Round-about\n" 
+		       "H	Hospital\n" 
+		       "P	Police Station\n" 
+		       "S	Shop\n" 
+		       "&	Debris\n");
+		pause();
+	}
+}
+
+//// TILE INFO
+//	- gives the user information about the tile
+void tile_info(char map[20][20], int position[2])
+{
+
+	char current_tile = map[position[0]][position[1]];
+
+	printf("tile: %c\n", current_tile);
+
+	switch (current_tile)
+	{
+		case ' ':
+			printf("You are standing in the middle of nowhere.");
+			break;
+		case '=':
+			printf("You are currently on a road that goes from the West to East end of the city.\n");
+			break;
+		case '|':
+			printf("You are currently on a road that goes from the North to South end of the city.\n");
+			break;
+		case '+':
+			printf("You are at an intersection.\n");
+			break;
+		case 'o':
+			printf("You are at a round-a-bout.\n");
+			break;
+		case 'H':
+			printf("You are standing in front of a city hospital.\n");
+			break;
+		case 'P':
+			printf("You are standing outside a city police station.\n");
+			break;
+		case 'S':
+			printf("You are outside a shop.\n");
+			break;
+		case '&':
+			printf("You are standing in a pile of debris.\n");
+			break;
+		default:
+			printf("Map tile not recognized.\n");
+
+	}
+
+	pause();
+}
+
+//// WALK
+//	- "walk [direction]" command
+//	- moves character depending on the direction
+int move(char *direction, int position[2])
+{
+	//printf("Direction in walk(): %s\n", direction);
+
+	if (direction == NULL)
+	{
+		printf("Please specify a direction.\n");
+		pause();
+		return 1;
+	}
+	if (((strcmp("up", direction) == 0) || (strcmp("north", direction) == 0)) && (position[0] != 0))
+		position[0]--;
+	else if (((strcmp("down", direction) == 0) || (strcmp("south", direction) == 0)) && (position[0] != 19))
+		position[0]++;
+	else if (((strcmp("left", direction) == 0) || (strcmp("west", direction) == 0)) && (position[1] != 0))
+		position[1]--;
+	else if (((strcmp("right", direction) == 0) || (strcmp("east", direction) == 0)) && (position[1] != 19))
+		position[1]++;
+	else
+	{
+		printf("Direction not recognized\n");
+		pause();
+	}
+
+	return 0;
+
+}
+
+////////////////////
+
 // check_command
 //	- checks the command against the programmed commands
-int check_command(char *word[100], char map[20][20], int char_position[2])
+int check_command(char *word[100], char map[20][20], int position[2], int health[2])
 {
 
 	//printf("check_command word[0]: %s\n", word[0]);
@@ -221,15 +335,18 @@ int check_command(char *word[100], char map[20][20], int char_position[2])
 
 	if (word[0] == NULL)
 		return 1;
-	//MAP
+	// ENTER
+	else if (strcmp("enter", word[0]) == 0)
+		enter(map, position, health);
+	// MAP
 	else if (strcmp("map", word[0]) == 0)
-		print_map(map, char_position, word);
-	//WALK
-	else if (strcmp("walk", word[0]) == 0)
-		move(word[1], char_position);
-	//TILE INFO
+		print_map(map, position, word);
+	// TILE INFO
 	else if ((strcmp("tile", word[0]) == 0) && (strcmp("info", word[1]) == 0))
-		tile_info(map, char_position);
+		tile_info(map, position);
+	// WALK
+	else if (strcmp("walk", word[0]) == 0)
+		move(word[1], position);
 	else
 	{
 		printf("Command not recognized.\n");
@@ -242,36 +359,39 @@ int check_command(char *word[100], char map[20][20], int char_position[2])
 int main(void)
 {
 
-	//struct gamestate state {,{0, 0}};
-
-	// Map
-	char map[20][20] =
+	struct playercharacteristics player = 
 	{
-	{"+=======+==========+"},
-	{"|       |          |"},
-	{"|       |          |"},
-	{"|=======+==========|"},
-	{"|       |          |"},
-	{"|       |          |"},
-	{"|       +==========|"},
-	{"|       |          |"},
-	{"|       |          |"},
-	{"|       +====+     |"},
-	{"|=======+    |     |"},
-	{"|       |    |     |"},
-	{"|       |    +=====|"},
-	{"|       |          |"},
-	{"|=======+          |"},
-	{"|                  |"},
-	{"|         o====+===|"},
-	{"|              |   |"},
-	{"|              |   |"},
-	{"+==============+===+"}
+		// Position
+		{0, 0},
+		// Map
+		{
+			{"+=======+==========+"},
+			{"|       |          |"},
+			{"|       |          |"},
+			{"|=======+==========|"},
+			{"|       |PP        |"},
+			{"|       |PP        |"},
+			{"|       +==========|"},
+			{"|       |HHHH      |"},
+			{"|       |HH        |"},
+			{"|       +====+     |"},
+			{"|=======+    |     |"},
+			{"|       |    |SS   |"},
+			{"|       |    +=====|"},
+			{"|       |          |"},
+			{"|=======+          |"},
+			{"|                  |"},
+			{"|         o====+===|"},
+			{"|   PPP        |   |"},
+			{"|   PPP        |   |"},
+			{"+==============+===+"}
+		},
+		
+		// Health (actual, full)
+		{100, 100},
+		
 	};
 
-	// Position
-	int char_position[2] = {0, 0};
-	
 
 	// Command
 	char command[100];
@@ -286,16 +406,19 @@ int main(void)
 
 		clearscr();
 
-		printf("%d, %d \n", char_position[0], char_position[1]);
+		printf("%d, %d \n", player.position[0], player.position[1]);
 		get_command(command);
 
 		// check for "exit" command.
 		if ((strcmp("exit", command) == 0) || (strcmp("quit", command) == 0))
+		{
+			printf("Good bye!");
+			pause();
 			return 0;
+		}
 
 		split_command(command, word);
-
-		check_command(word, map, char_position);
+		check_command(word, player.map, player.position, player.health);
 
 	} while (1);
 
